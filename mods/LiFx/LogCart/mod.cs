58ce9d8ebcdf4da2ebb5e550c0136ef793d8806d
matty5350 <matthew_uk227@hotmail.com>
2022-12-17 21:30:13 +0000

    patch Update

--- mods/LiFx/LogCart/mod.cs
+++ mods/LiFx/LogCart/mod.cs
@@ -23,11 +23,11 @@ package LogCart
         
         // Register new objects
         LiFx::registerObjectsTypes(LogCart::ObjectsTypesCart(), LogCart);
-        LiFx::registerObjectsTypes(LogCart::ObjectsTypesHarnessedLogCart(), LogCart);
+    //    LiFx::registerObjectsTypes(LogCart::ObjectsTypesHarnessedLogCart(), LogCart);
 
     }
     function LogCart::version() {
-        return "0.0.1";
+        return "1.0.0";
     }
         function LogCart::Datablock() {
         exec ("yolauncher/modpack/mods/LiFx/LogCart/Transport.cs");
@@ -38,7 +38,7 @@ package LogCart
         {
             id = 3016; // has to be globally unique, please reserve ids here: https://www.lifxmod.com/info/object-id-list/
             ObjectName = "Log Cart";
-            ParentID = 77;
+            ParentID = 77; //1902
             IsContainer = 1;
             IsMovableObject = 1;
             IsUnmovableobject = 0;
@@ -50,48 +50,16 @@ package LogCart
             Length = 7; 
             MaxStackSize = 0;
             UnitWeight = 10000;
-            BackgrndImage = 'art\\images\\universal';
+            BackgrndImage = "art\\\\images\\\\universal";
             WorkAreaTop = 0;
             WorkAreaLeft = 0;
             WorkAreaWidth = 0;
             WorkAreaHeight = 0;
             BtnCloseTop = 0;
             BtnCloseLeft = 0;
-            FaceImage = 'art\\2D\\Objects\\trader_cart.png';
+            FaceImage = "yolauncher/modpack/mods/LiFx/LogCart/2D/Items/cart/logcart.png";
             Description = '';
-            BasePrice = 135600;
-            OwnerTimeout = 30;
-            AllowExportFromRed = 0;
-            AllowExportFromGreen = 0;
-        };
-    }
-        function LogCart::ObjectsTypesHarnessedLogCart() {
-        return new ScriptObject(ObjectsTypesHarnessedLogCart : ObjectsTypes)
-        {
-            id = 3018; // has to be globally unique, please reserve ids here: https://www.lifxmod.com/info/object-id-list/
-            ObjectName = "Harnessed Log Cart";
-            ParentID = 77;
-            IsContainer = 1;
-            IsMovableObject = 1;
-            IsUnmovableobject = 0;
-            IsTool = 0;
-            IsDevice = 0;
-            IsDoor = 0;
-            IsPremium = 0;
-            MaxContSize = 4000000;
-            Length = 7; 
-            MaxStackSize = 0;
-            UnitWeight = 10000;
-            BackgrndImage = 'art\\images\\universal';
-            WorkAreaTop = 0;
-            WorkAreaLeft = 0;
-            WorkAreaWidth = 0;
-            WorkAreaHeight = 0;
-            BtnCloseTop = 0;
-            BtnCloseLeft = 0;
-            FaceImage = 'art\\2D\\Objects\\trader_cart.png';
-            Description = '';
-            BasePrice = 100;
+            BasePrice = 265600;
             OwnerTimeout = 30;
             AllowExportFromRed = 0;
             AllowExportFromGreen = 0;
@@ -100,7 +68,7 @@ package LogCart
 
   function LogCart::dbChanges() {
            ///////////////////////////////////////Recipe /////////////////////////////////////////////
-    dbi.Update("INSERT IGNORE `recipe` VALUES (1093,'LogCart','',36,8,90,3016,30,1,0,0, 'art\\2D\\Recipes\\horse_cart.png')");
+    dbi.Update("INSERT IGNORE `recipe` VALUES (1093,'LogCart','',36,8,90,3016,30,1,0,0, 'yolauncher/modpack/mods/LiFx/LogCart/2D/Recipies/LogCart.png')");
    
      ///////////////////////////////////////Recipe Requirements /////////////////////////////////////////////
 
